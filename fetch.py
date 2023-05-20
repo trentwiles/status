7a0dfb74e28d70cf0cb269fc29153dbffae8bd60
RiversideRocks <wilest7@gmail.com>
2023-05-19 21:13:52 -0400

    tonigts updates

--- fetch.py
+++ fetch.py
@@ -1,10 +1,18 @@
 import requests
 import json
+import os
 
 def fetch():
     with open("servers.json", 'r') as s:
+        load = []
         x = json.load(s)
         for z in x:
-            api = requests.get(z["endpoint"] + "/data", headers={"User-agent": "trent"})
+            api = requests.get("http://" + z["endpoint"] + "/", headers={"User-agent": "trent"})
+            x = {"server": (z["desc"] + " (" + z["IATA"] + ")"), "metrics": api.json()}
+            load.append(x)
+    os.remove("cache.json")
+    with open("cache.json", 'a') as w:
+        w.write(json.dumps(load))
+        w.close()
 
 fetch()
